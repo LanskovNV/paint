b635390c728021069f7c28eecf2365973b462c07
LanskovNV <leinssniel@gmail.com>
2017-05-29 09:31:23 +0300

    problem with drawing...

--- paint_callbacks.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAINT_CALLBACKS_H
-#define PAINT_CALLBACKS_H
-
-/* functions */
-void Keyboard(unsigned char Key, int MouseX, int MouseY);
-void Display(void);
-void Mouse(int button, int state, int x, int y);
-void Motion(int x, int y);
-#endif // PAINT_CALLBACKS_H
